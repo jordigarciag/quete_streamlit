23582a4037d7cf2a4377663dc6796bfa21586d43
jordigarciag <jordigarciagoma@gmail.com>
2024-12-11 11:32:30 +0100

    Update quete_streamlit_partie_3.py

--- quete_streamlit_partie_3.py
+++ quete_streamlit_partie_3.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_authenticator import Authenticate
+from streamlit_option_menu import option_menu
 
 # Nos données utilisateurs doivent respecter ce format
 
@@ -33,48 +34,56 @@ authenticator.login()
 # Gestion de l'accès
 
 def accueil():
-      st.title("Bienvenu sur le contenu réservé aux utilisateurs connectés")
+      st.title("Bienvenu sur le contenu réservé aux data analysts en recherche de style")
+      
+
+def accueil_photo():
+      st.title("Choisis tes chaussures pour ton rendez-vous data")
+      
 
 
 if st.session_state["authentication_status"]:
-  accueil()
-  # Le bouton de déconnexion
-  authenticator.logout("Déconnexion")
+    with st.sidebar:
+        st.write("Bienvenu")
+        # Création du menu qui va afficher les choix qui se trouvent dans la variable options
+        selection = option_menu(
+                    menu_title=None,
+                    options = ["Accueil", "Photos"]
+                )
+        # Le bouton de déconnexion
+        authenticator.logout("Déconnexion")
+        
+    if selection == "Accueil" : 
+        accueil()
+        st.image("data_analyst.jpg")
+    
+    elif selection == "Photos" :
+        accueil_photo()
+        # ici autre photo
 
-elif st.session_state["authentication_status"] is False:
-    st.error("L'username ou le password est/sont incorrect")
-elif st.session_state["authentication_status"] is None:
-    st.warning('Les champs username et mot de passe doivent être remplie')
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            #img
+            st.image("sport.jpg")
+            #text
+            st.write("Sport")
+        with col2:
+            #img
+            st.image("chaussures.jpg")
+            #text
+            st.write("Classe")
+        with col3:
+            #img
+            st.image("bottines.jpg")
+            #text
+            st.write("Bottines")
+    
+   
+
+    if st.session_state["authentication_status"] is False:
+        st.error("L'username ou le password est/sont incorrect")
+    if st.session_state["authentication_status"] is None:
+        st.warning('Les champs username et mot de passe doivent être remplie')
 
 
 
-
-# Création du menu
-
-import streamlit as st
-# Importation du module
-from streamlit_option_menu import option_menu
-
-# Création du menu qui va afficher les choix qui se trouvent dans la variable options
-selection = option_menu(
-            menu_title=None,
-            options = ["Accueil", "Photos"]
-        )
-
-
-
-# On indique au programme quoi faire en fonction du choix
-if selection == "Accueil":
-    st.write("Bienvenue sur la page d'accueil !")
-elif selection == "Photos":
-    st.write("Bienvenue sur mon album photo")
-# ... et ainsi de suite pour les autres pages
-
-
-
-# -------------------------------------------------- 
-
-# Pour runer il faut faire :
-# streamlit run quete_streamlit_partie_3.py
-
-# -------------------------------------------------- 
